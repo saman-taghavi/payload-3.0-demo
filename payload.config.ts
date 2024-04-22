63c24fbdb5f9b1f5e2f05b3fb78b6a9058e18b5c
saman-taghavi <samantaghavisani@gmail.com>
2024-04-22 17:37:17 +0330

    add: create env
    chore: use double qoutes
    
    chore: add bun to ignore
    
    chore: remove .idea
    
    fix: format with lint
    
    fix: use postgres
    
    chore: liara configs
    
    fix (gitignore): Remove ignored files
    
    fix (gitignore): Remove ignored files
    
    add: docker
    
    fix: only use pnpm in build of docker
    
    fix: remove build from lockfile
    
    fix: seperate corepack
    
    fix: use npm instead of pnpm
    
    fix: use pnpm again
    
    fix: use ir-proxy registery
    
    fix: use npm again | IR fuckers
    
    fix: switch to yarn ?
    
    fix: set yarn registery
    
    fix: use pnpm | server dns settigns
    
    fix: use npm  | tired of this shit
    
    fix: use pnpm | remove all dns shits
    
    fix: fogot to enable pnpm | :) ✅

--- payload.config.ts
+++ payload.config.ts
@@ -1,6 +1,6 @@
-import path from 'path'
-// import { postgresAdapter } from '@payloadcms/db-postgres'
-import { en } from 'payload/i18n/en'
+import path from "path"
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { en } from "payload/i18n/en"
 import {
   AlignFeature,
   BlockQuoteFeature,
@@ -18,12 +18,12 @@ import {
   RelationshipFeature,
   UnorderedListFeature,
   UploadFeature,
-} from '@payloadcms/richtext-lexical'
+} from "@payloadcms/richtext-lexical"
 //import { slateEditor } from '@payloadcms/richtext-slate'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { buildConfig } from 'payload/config'
-import sharp from 'sharp'
-import { fileURLToPath } from 'url'
+// import { mongooseAdapter } from "@payloadcms/db-mongodb"
+import { buildConfig } from "payload/config"
+import sharp from "sharp"
+import { fileURLToPath } from "url"
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -33,7 +33,7 @@ export default buildConfig({
   editor: lexicalEditor(),
   collections: [
     {
-      slug: 'users',
+      slug: "users",
       auth: true,
       access: {
         delete: () => false,
@@ -42,44 +42,44 @@ export default buildConfig({
       fields: [],
     },
     {
-      slug: 'pages',
+      slug: "pages",
       admin: {
-        useAsTitle: 'title',
+        useAsTitle: "title",
       },
       fields: [
         {
-          name: 'title',
-          type: 'text',
+          name: "title",
+          type: "text",
         },
         {
-          name: 'content',
-          type: 'richText',
+          name: "content",
+          type: "richText",
         },
       ],
     },
     {
-      slug: 'media',
+      slug: "media",
       upload: true,
       fields: [
         {
-          name: 'text',
-          type: 'text',
+          name: "text",
+          type: "text",
         },
       ],
     },
   ],
-  secret: process.env.PAYLOAD_SECRET || '',
+  secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
+    outputFile: path.resolve(dirname, "payload-types.ts"),
   },
-  // db: postgresAdapter({
-  //   pool: {
-  //     connectionString: process.env.POSTGRES_URI || ''
-  //   }
-  // }),
-  db: mongooseAdapter({
-    url: process.env.MONGODB_URI || '',
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URI || ''
+    }
   }),
+  // db: mongooseAdapter({
+  //   url: process.env.MONGODB_URI || "",
+  // }),
 
   /**
    * Payload can now accept specific translations from 'payload/i18n/en'
@@ -91,23 +91,23 @@ export default buildConfig({
 
   admin: {
     autoLogin: {
-      email: 'dev@payloadcms.com',
-      password: 'test',
+      email: "dev@payloadcms.com",
+      password: "test",
       prefillOnly: true,
     },
   },
   async onInit(payload) {
     const existingUsers = await payload.find({
-      collection: 'users',
+      collection: "users",
       limit: 1,
     })
 
     if (existingUsers.docs.length === 0) {
       await payload.create({
-        collection: 'users',
+        collection: "users",
         data: {
-          email: 'dev@payloadcms.com',
-          password: 'test',
+          email: "dev@payloadcms.com",
+          password: "test",
         },
       })
     }
